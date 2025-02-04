ec6c761efdfb0aee39427a81b11b7224e88ec42f
Nishant Kumpakha <nishantkumpakha@Nishants-MacBook-Air.local>
2025-02-04 11:42:38 +0545

    string extensions done

---
+++ extenmethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a string:");
+        string s = Console.ReadLine();
+
+        // Reverse the string
+        Console.WriteLine(s.ReverseString());
+
+        // Convert the string to title case
+        Console.WriteLine(s.ToTitleCase());
+
+        // Get the vowels in the string
+        Console.WriteLine(s.GetVowels());
+
+        // Calculate the age of a person born on the entered date
+        Console.WriteLine("Please enter your date of birth (yyyy-MM-dd):");
+        DateTime dateOfBirth = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine(dateOfBirth.Age());
+    }
+}
---
+++ extenmethods/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+public static class StringExtensions
+{
+    public static string ReverseString(this string s)
+    {
+        char[] arr = s.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
+    }
+
+    public static string ToTitleCase(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+        else
+        {
+            return char.ToUpper(input[0]) + input.Substring(1);
+        }
+    }
+
+    public static string GetVowels(this string s)
+    {
+        string vowels = "aeiouAEIOU";
+        return new string(s.Where(c => vowels.Contains(c)).ToArray());
+    }
+
+    public static int Age(this DateTime dateOfBirth)
+    {
+        DateTime today = DateTime.Today;
+        int age = today.Year - dateOfBirth.Year;
+        if (today < dateOfBirth.AddYears(age)) age--;
+        return age;
+    }
+}
